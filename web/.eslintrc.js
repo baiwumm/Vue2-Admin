53005de0bd01d5e759018e28a00e190d8ad375e0
baiwumm <baiwumm@foxmail.com>
2024-10-30 15:50:33 +0800

    feat(defaultSettings): 配置主题和 Logo 等项目信息

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -3,10 +3,7 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/strongly-recommended',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/strongly-recommended', '@vue/standard'],
   rules: {
     'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
@@ -15,10 +12,10 @@ module.exports = {
     'vue/max-attributes-per-line': [
       2,
       {
-        'singleline': 5,
-        'multiline': {
-          'max': 1,
-          'allowFirstLine': false
+        singleline: 5,
+        multiline: {
+          max: 1,
+          allowFirstLine: false
         }
       }
     ],
@@ -33,40 +30,38 @@ module.exports = {
     'vue/html-closing-bracket-newline': 0,
     'vue/no-parsing-error': 0,
     'no-tabs': 0,
-    'quotes': [
+    quotes: [
       2,
       'single',
       {
-        'avoidEscape': true,
-        'allowTemplateLiterals': true
+        avoidEscape: true,
+        allowTemplateLiterals: true
       }
     ],
-    'semi': [
+    semi: [
       2,
       'never',
       {
-        'beforeStatementContinuationChars': 'never'
+        beforeStatementContinuationChars: 'never'
       }
     ],
     'no-delete-var': 2,
     'prefer-const': [
       2,
       {
-        'ignoreReadBeforeAssign': false
+        ignoreReadBeforeAssign: false
       }
     ],
     'template-curly-spacing': 'off',
-    'indent': 'off'
+    indent: 'off',
+    'comma-dangle': ['error', 'never']
   },
   parserOptions: {
     parser: 'babel-eslint'
   },
   overrides: [
     {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
       env: {
         jest: true
       }
--- web/src/config/defaultSettings.js
+++ web/src/config/defaultSettings.js
@@ -14,7 +14,7 @@
 
 export default {
   navTheme: 'dark', // theme for nav menu
-  primaryColor: '#1890ff', // '#F5222D', // primary color of ant design
+  primaryColor: '#F5222D', // '#F5222D', // primary color of ant design
   layout: 'sidemenu', // nav menu position: `sidemenu` or `topmenu`
   contentWidth: 'Fluid', // layout of content: `Fluid` or `Fixed`, only works when layout is topmenu
   fixedHeader: false, // sticky header
@@ -23,7 +23,7 @@ export default {
   menu: {
     locale: true
   },
-  title: 'Ant Design Pro',
+  title: 'Vue2 Admin',
   pwa: false,
   iconfontUrl: '',
   production: process.env.NODE_ENV === 'production' && process.env.VUE_APP_PREVIEW !== 'true'
