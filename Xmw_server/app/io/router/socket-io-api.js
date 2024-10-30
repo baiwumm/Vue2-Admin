45721cc8b0804e9abea05376857b60be064a86a8
baiwumm <baiwumm@foxmail.com>
2024-10-30 11:07:26 +0800

    refactor: 初始化项目

--- Xmw_server/app/io/router/socket-io-api.js
+++
@@ -1,6 +0,0 @@
-
-module.exports = app => {
-    const { router, controller, io } = app;
-    io.of('/').route('announcement', io.controller.announcement.jieshou)
-    io.of('/').route('chat', io.controller.chatRoom.chat)
-}
--- Xmw_web/src/views/toolpage/multistage/menu2/index.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <page-header-wrapper>
-        <a-alert :closable="false" message="菜单2"></a-alert>
-    </page-header-wrapper>
-</template>
