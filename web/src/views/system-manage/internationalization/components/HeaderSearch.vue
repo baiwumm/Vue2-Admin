27cf45003ef390412e2ccd88d3018df1d47dcf1a
baiwumm <baiwumm@foxmail.com>
2024-11-04 10:31:28 +0800

    style(internalization): 顶部搜索样式优化

--- web/src/views/system-manage/internationalization/components/HeaderSearch.vue
+++ web/src/views/system-manage/internationalization/components/HeaderSearch.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="table-page-search-wrapper">
     <a-form layout="inline">
-      <a-row :gutter="[16, 16]" align="middle">
-        <a-col :md="6" :sm="12">
+      <a-row :gutter="16" align="middle">
+        <a-col :sm="24" :md="12" :lg="8" :xl="6" :xxl="6">
           <a-form-item :label="I18nInternationalization('name')">
             <a-input :placeholder="I18nEntry(I18nInternationalization('name'))" allowClear v-model="queryForm.name" />
           </a-form-item>
         </a-col>
-        <a-col :md="6" :sm="12">
+        <a-col :sm="24" :md="12" :lg="8" :xl="6" :xxl="6">
           <a-form-item :label="I18nInternationalization('zh-CN')">
             <a-input :placeholder="I18nEntry(I18nInternationalization('zh-CN'))" allowClear v-model="queryForm.zhCN" />
           </a-form-item>
         </a-col>
-        <a-col :md="6" :sm="24">
+        <a-col :sm="24" :md="12" :lg="8" :xl="6" :xxl="6">
           <a-form-item :label="$t(I18nGlobal.createdAt)">
             <a-range-picker v-model="queryForm.createdAt" valueFormat="YYYY-MM-DD" />
           </a-form-item>
         </a-col>
-        <a-col :md="6" :sm="12">
+        <a-col :sm="24" :md="12" :lg="8" :xl="6" :xxl="6">
           <span class="table-page-search-submitButtons">
             <a-space>
               <a-button type="primary" @click="query">{{ $t(I18nGlobal.search) }}</a-button>
