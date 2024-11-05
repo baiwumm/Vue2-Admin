12e70db0c2c42bfb33bffeac45c0bf8278373a02
baiwumm <baiwumm@foxmail.com>
2024-11-05 17:22:09 +0800

    feat(GlobalFooter): 完善底部版权信息

--- web/src/components/GlobalFooter/index.vue
+++ web/src/components/GlobalFooter/index.vue
@@ -1,23 +1,41 @@
 <template>
   <global-footer class="footer custom-render">
     <template v-slot:links>
-      <a href="https://www.github.com/vueComponent/pro-layout" target="_blank">Pro Layout</a>
-      <a href="https://www.github.com/vueComponent/ant-design-vue-pro" target="_blank">Github</a>
-      <a href="https://www.github.com/sendya/" target="_blank">@Sendya</a>
+      <a title="Vue3 Admin" href="https://github.com/baiwumm/Vue3-Admin/" target="_blank">Vue3 Admin</a>
+      <a title="github" href="https://github.com/baiwumm/Vue2-Admin/" target="_blank"><a-icon type="github" /></a
+      ><a title="Xmw Admin" href="https://github.com/baiwumm/Xmw-Admin/" target="_blank">Xmw Admin</a>
     </template>
     <template v-slot:copyright>
-      <a href="https://github.com/vueComponent" target="_blank">vueComponent</a>
-    </template>
+      Copyright © {{ dayjs().year() }} {{ defaultSettings.title }} by {{ pkg.author.email }}
+      <a
+        href="https://beian.miit.gov.cn/"
+        target="_blank"
+        style="color: rgba(0, 0, 0, 0.45); font-size: 14px; margin-left: 10px"
+      >
+        粤ICP备2023007649号
+      </a></template
+    >
   </global-footer>
 </template>
 
 <script>
 import { GlobalFooter } from '@ant-design-vue/pro-layout'
+import dayjs from 'dayjs'
 
+import defaultSettings from '@/config/defaultSettings'
+
+import pkg from '../../../package.json'
 export default {
   name: 'ProGlobalFooter',
   components: {
     GlobalFooter
+  },
+  data() {
+    return {
+      dayjs,
+      defaultSettings,
+      pkg
+    }
   }
 }
 </script>
