296121854bc1e6424e0838d2dc4c746573db0341
baiwumm <baiwumm@foxmail.com>
2024-11-08 18:05:27 +0800

    feat(api): 新增消息公告模块接口

---
+++ web/src/api/administrative/message.js
@@ -0,0 +1,54 @@
+import request from '@/utils/request'
+
+/**
+ * @description: 获取消息公告列表
+ */
+export function getMessageList(parameter) {
+  return request({
+    url: '/administrative/message',
+    method: 'get',
+    params: parameter
+  })
+}
+
+/**
+ * @description: 创建消息公告
+ */
+export function addMessage(parameter) {
+  return request({
+    url: '/administrative/message',
+    method: 'post',
+    data: parameter
+  })
+}
+
+/**
+ * @description: 更新消息公告
+ */
+export function updateMessage({ id, ...parameter }) {
+  return request({
+    url: `/administrative/message/${id}`,
+    method: 'put',
+    data: parameter
+  })
+}
+
+/**
+ * @description: 删除消息公告
+ */
+export function delMessage(id) {
+  return request({
+    url: `/administrative/message/${id}`,
+    method: 'delete'
+  })
+}
+
+/**
+ * @description: 修改置顶状态
+ */
+export function changePinned(id) {
+  return request({
+    url: `/administrative/message/${id}`,
+    method: 'patch'
+  })
+}
