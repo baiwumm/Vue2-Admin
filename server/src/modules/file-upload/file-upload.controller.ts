0e80e96fcad3837c1b8eb237a60ebd88f01ccd18
baiwumm <baiwumm@foxmail.com>
2024-11-08 11:18:39 +0800

    feat(FileUploadController): 设置文件上传生产和开发环境域名

--- server/src/modules/file-upload/file-upload.controller.ts
+++ server/src/modules/file-upload/file-upload.controller.ts
@@ -2,14 +2,13 @@
  * @Author: 白雾茫茫丶<baiwumm.com>
  * @Date: 2024-07-29 14:24:19
  * @LastEditors: 白雾茫茫丶<baiwumm.com>
- * @LastEditTime: 2024-08-02 10:01:55
+ * @LastEditTime: 2024-11-08 11:02:12
  * @Description: FileUploadController
  */
-import { Controller, Post, Req, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiBearerAuth, ApiBody, ApiConsumes, ApiHeader, ApiOkResponse, ApiTags } from '@nestjs/swagger';
-import { Request } from 'express';
 
 import { responseMessage } from '@/utils';
 
@@ -36,12 +35,10 @@ export class FileUploadController {
     description: '单个文件上传',
     type: FileUploadDto,
   })
-  uploadFile(@UploadedFile() file: Express.Multer.File, @Req() req: Request): CommonType.Response<Express.Multer.File> {
-    // 获取客户端域名端口
-    const hostname = req.headers['x-forwarded-host'] || req.hostname;
-    const port = req.headers['x-forwarded-port'] || req.socket.localPort;
-    const protocol = req.headers['x-forwarded-proto'] || req.protocol;
-    file.path = `${protocol}://${hostname}:${port}/static${file.path.replace(/\\/g, '/').replace(/upload/g, '')}`;
+  uploadFile(@UploadedFile() file: Express.Multer.File): CommonType.Response<Express.Multer.File> {
+    // 判断环境获取域名
+    const hostname = process.env.NODE_ENV === 'production' ? process.env.DOMAIN_NAME : process.env.DEV_DOMAIN_NAME;
+    file.path = `${hostname}/static${file.path.replace(/\\/g, '/').replace(/upload/g, '')}`;
     return responseMessage(file);
   }
 }
