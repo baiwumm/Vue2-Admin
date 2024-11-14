265cd6e3f7c3d6d817465ba8071c3ca1fe10e5af
baiwumm <baiwumm@foxmail.com>
2024-11-14 16:41:46 +0800

    perf(Prisma): Organization 模型去掉 icon 字段

---
+++ server/prisma/migrations/20241114084039_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `icon` on the `Organization` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Organization" DROP COLUMN "icon";
